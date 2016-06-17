9cbafa6f93948f67e87ae32fc948d3890ca7952b
sejour <shuka.takakuma@gmail.com>
2016-06-18 07:12:15 +0900

    [add] ファイルへの書き込み処理を追加

--- src/shuka/takakuma/pnc/ProperNounCollector.java
+++ src/shuka/takakuma/pnc/ProperNounCollector.java
@@ -3,12 +3,20 @@ package shuka.takakuma.pnc;
 import java.util.List;
 
 import shuka.takakuma.net.HttpClient;
+import shuka.takakuma.wikipedia.WikipediaApi;
+import shuka.takakuma.wikipedia.model.Category;
 
 public class ProperNounCollector {
 
 	public static void main(String[] args) {
 		ProperNounCollector pnc = new ProperNounCollector();
-		pnc.collect("https://ja.wikipedia.org/wiki/曲名一覧");
+		pnc.collect("https://ja.wikipedia.org/wiki/日本の企業一覧_(情報・通信)");
+		//https://ja.wikipedia.org/wiki/曲名一覧
+		//https://ja.wikipedia.org/wiki/Category:音楽のジャンル
+		//https://ja.wikipedia.org/wiki/新語・流行語大賞
+		//https://ja.wikipedia.org/wiki/日本の地方公共団体一覧
+		//https://ja.wikipedia.org/wiki/日本の企業一覧_(サービス)
+		//https://ja.wikipedia.org/wiki/日本の企業一覧_(情報・通信)
 	}
 
 	public ProperNounCollector() {
@@ -20,20 +28,23 @@ public class ProperNounCollector {
 	}
 
 	public void collect(String url) {
-		System.out.println("[start]");
 		HttpClient.get(url, (statusCode, reader) -> {
 			TextNormalizer normalizer = new TextNormalizer();
+			Writer writer = new Writer();
 			String line;
 			int count = 0;
+			System.out.println("[start]");
             while ((line = reader.readLine()) != null) {
-            	//System.out.println(line);
-            	List<String> tokens = ProperNounExtractor.extract(normalizer.normalizeWebText(line));
-            	for (String token : tokens) {
-            		System.out.println(token);
+            	List<String> words = ProperNounExtractor.extract(normalizer.normalizeWebText(line));
+            	for (String word : words) {
             		++count;
+            		//if (count < 7054) continue;
+            		System.out.printf("[%d] %s\n", count, word);
+            		List<Category> categories = WikipediaApi.getCategoriesFromTitle(word);
+            		writer.write(word, categories);
             	}
-            	//System.out.println("#####################################");
             }
+            writer.close();
             System.out.printf("[end] (%d)\n", count);
 		});
 	}
