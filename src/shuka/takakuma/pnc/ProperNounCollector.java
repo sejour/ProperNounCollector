5dbc3afb1dfffc5f7b4f6dcbc7b1c32060b57ad1
sejour <shuka.takakuma@gmail.com>
2016-06-18 20:25:52 +0900

    [add] data

--- src/shuka/takakuma/pnc/ProperNounCollector.java
+++ src/shuka/takakuma/pnc/ProperNounCollector.java
@@ -10,13 +10,21 @@ public class ProperNounCollector {
 
 	public static void main(String[] args) {
 		ProperNounCollector pnc = new ProperNounCollector();
-		pnc.collect("https://ja.wikipedia.org/wiki/日本の企業一覧_(情報・通信)");
+		pnc.collect("https://ja.wikipedia.org/wiki/ファッションブランド一覧");
 		//https://ja.wikipedia.org/wiki/曲名一覧
 		//https://ja.wikipedia.org/wiki/Category:音楽のジャンル
 		//https://ja.wikipedia.org/wiki/新語・流行語大賞
 		//https://ja.wikipedia.org/wiki/日本の地方公共団体一覧
 		//https://ja.wikipedia.org/wiki/日本の企業一覧_(サービス)
 		//https://ja.wikipedia.org/wiki/日本の企業一覧_(情報・通信)
+		//https://ja.wikipedia.org/wiki/日本のテレビ番組一覧
+		//https://ja.wikipedia.org/wiki/学問の一覧
+		//https://ja.wikipedia.org/wiki/日本学術会議協力学術研究団体
+		//https://ja.wikipedia.org/wiki/Category:日本のギネス世界記録
+		//https://ja.wikipedia.org/wiki/コンピュータ用語一覧
+		//https://ja.wikipedia.org/wiki/日本の記念日一覧
+		//https://ja.wikipedia.org/wiki/超高層ビルの一覧
+		//https://ja.wikipedia.org/wiki/ファッションブランド一覧
 	}
 
 	public ProperNounCollector() {
@@ -38,7 +46,6 @@ public class ProperNounCollector {
             	List<String> words = ProperNounExtractor.extract(normalizer.normalizeWebText(line));
             	for (String word : words) {
             		++count;
-            		//if (count < 7054) continue;
             		System.out.printf("[%d] %s\n", count, word);
             		List<Category> categories = WikipediaApi.getCategoriesFromTitle(word);
             		writer.write(word, categories);
